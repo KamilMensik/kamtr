395a01dafc5fd7e8485a1cea6dd1e5cc525dd73f
KamilMensik <kamilekmensik@gmail.com>
2022-02-07 14:43:57 +0100

    Pridani pg

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts
 
   def self.find_for_database_authentication warden_condition
