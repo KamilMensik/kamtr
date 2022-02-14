8d87ea91560c6752a24a35885fd89b553bff6e16
KamilMensik <kamilekmensik@gmail.com>
2022-02-14 17:47:48 +0100

    a minor fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,21 +28,17 @@ class PostsController < ApplicationController
     def like
         @post = Post.find(params[:id])
         Like.create(user_id:current_user.id, post_id: @post.id)
-        back
+        redirect_back(fallback_location: root_path)
     end
     
     def unlike
         @like = Like.where(post_id: params[:id],user_id: current_user.id).first
         @like.destroy
-        back
+        redirect_back(fallback_location: root_path)
     end
 
     private
     def post_params
         params.require(:post).permit(:user_id,:content,:is_edited)
-    end
-
-    def back
-        redirect_back(fallback_location: root_path)
     end
 end
